b6d687f47059cae22c4f7e620085006e3f460519
Semih Guresci <semih.guresci@gmail.com>
2025-02-17 22:00:50 +0100

    Platform specific GtestCommon

--- tests/GTestCommon.hpp
+++ tests/GTestCommon.hpp
@@ -1,11 +1,15 @@
 #ifndef GTEST_COMMON_HPP
 #define GTEST_COMMON_HPP
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wanalyzer-malloc-leak"
-
 #include "gtest/gtest.h"
 
+#if defined(__clang__)  
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wanalyzer-malloc-leak"
+#endif
+
+#if defined(__clang__) 
 #pragma clang diagnostic pop
+#endif
 
 #endif
