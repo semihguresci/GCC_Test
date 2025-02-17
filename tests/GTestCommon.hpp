b646f4bb46a312d5174e564efcc5b22812c43269
Semih Guresci <semih.guresci@gmail.com>
2025-02-17 22:08:02 +0100

    warning removed

--- tests/GTestCommon.hpp
+++ tests/GTestCommon.hpp
@@ -6,10 +6,15 @@
 #if defined(__clang__)  
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wanalyzer-malloc-leak"
+#elif defined(_MSC_VER) 
+
 #endif
 
-#if defined(__clang__) 
+
+
+#if defined(__clang__)  
 #pragma clang diagnostic pop
+#elif defined(_MSC_VER) 
 #endif
 
 #endif
